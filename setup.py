b272d5deb5b71127f9618254b31fc5d50768b5c2
Karn_Gupta <karnguptaprivate123@gmail.com>
2024-09-11 16:55:21 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Karn',
+    author_email='karnguptaprivate123@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
